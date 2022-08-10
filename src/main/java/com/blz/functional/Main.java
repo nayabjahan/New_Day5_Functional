63318baaea6a9477aa3d0f123db27e0fe72a7aae
nayab jahan <nayabzafir@gmail.com>
2022-08-10 22:19:54 +0530

    Added:Quadratic equation

---
+++ src/main/java/com/blz/functional/Main.java
@@ -0,0 +1,10 @@
+package com.blz.functional;
+
+import static com.blz.functional.Functional.calculateRoots;
+
+public class Main {
+    public static void main(String args[]) {
+
+        calculateRoots(2,5,7);
+    }
+}
